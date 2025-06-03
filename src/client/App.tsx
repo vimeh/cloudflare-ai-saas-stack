d60ddead1b34fcd0610c12e5e234fd029fc727f9
Vijay Nandwani <nandwani.vijay96@gmail.com>
2025-06-03 02:03:22 +0200

    Adding Tailwind CSS v4 to the project

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
 }
