22cfbb6f753d2f4e856a6c2566200da94fd80a53
Vijay Nandwani <nandwani.vijay96@gmail.com>
2025-06-04 00:17:51 +0200

    Integrating Tanstack Query and Hono RPC

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,11 +1,16 @@
+import App from "@client/app";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "@client/app";
+
+const queryClient = new QueryClient();
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</StrictMode>,
 );
