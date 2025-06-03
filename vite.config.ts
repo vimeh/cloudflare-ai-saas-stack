6371e839d2f564cb2c3881f460d22bb503ec6ff2
Vijay Nandwani <nandwani.vijay96@gmail.com>
2025-06-03 02:22:25 +0200

    Setting Up Shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), cloudflare(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@client": path.resolve(__dirname, "./src/client"),
+      "@worker": path.resolve(__dirname, "./src/worker"),
+    },
+  },
 });
