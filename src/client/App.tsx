c29a431e26fb1b6daab5a7895d91d0f551ec652a
Vijay Nandwani <nandwani.vijay96@gmail.com>
2025-06-03 17:25:03 +0200

    Adding a shadcn component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@client/components/ui/button";
+
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">
+        Hello World
+        <Button>Click me!</Button>
+      </h1>
     </div>
   );
 }
