4cbd54c6e1ad7c55d4cf5bd85858ab95f7cb3882
Vinay Mehta <hi@vinaym.com>
2025-07-05 17:59:22 -0700

    `bunx --bun shadcn@latest migrate radix` (deps)

--- src/client/components/ui/label.tsx
+++ src/client/components/ui/label.tsx
@@ -1,5 +1,5 @@
 import type * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
+import { Label as LabelPrimitive } from "radix-ui"
 
 import { cn } from "@client/lib/utils"
 
